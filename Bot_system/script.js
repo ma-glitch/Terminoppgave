c14bf3345a94042a9fd86405661943d8aea35f98
Martin_T <Tangenmartin06@gmail.com>
2023-09-25 02:07:12 -0700

    Oppdatering kode
    oppdaterer og lager kode

--- Bot_system/script.js
+++ Bot_system/script.js
@@ -1,23 +1,49 @@
-var mySql = require('sync-mysql')
+var mySql = require("sync-mysql");
 
 var connection = new mySql({
-  host: 'localhost',
-  user: 'root',
-  password: 'Admin',
-  database: "BotDB"
+  host: "localhost",
+  user: "root",
+  password: "Admin",
+  database: "BotDB",
 });
 
+var botsyst = connection.query("SELECT * from Boter");
+
+var passord = connection.query("SELECT * from login");
 
 
+function login(){
+  var bruker = document.getElementById("bruker")
+  var passord = document.getElementById("passord")
+  
+  var loginnavn = "SELECT * from login WHERE " + bruker + "= '" + passord + "'";
 
-
-  function betalbtn() {
-  document.getElementById("betal").style.display = "block";
+var logetin = connection.query(loginnavn);
+  if (logetin < 1){
+    console.log("det fungerer å loge in");
+  } else if (logetin === 0){
+    console.log("ingen med det navnet");
   }
 
-   function openForm() {
-    document.getElementById("myForm").style.display = "block";
-   }
-   function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-   }
+}
+
+function displaymembers(){
+  var display = document.getElementById("liste")
+
+  var vis = connection.query("SELECT * from login")
+  
+  for (var i = 0; i < vis.length; i++) {
+
+  }
+}
+
+function betalbtn() {
+  document.getElementById("betal").style.display = "block";
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
