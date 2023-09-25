7cef04c29eb73978411ebaaa894d484a9c792d53
Martin_T <Tangenmartin06@gmail.com>
2023-09-25 02:49:18 -0700

    fike kode
    kode

--- Bot_system/script.js
+++ Bot_system/script.js
@@ -7,33 +7,39 @@ var connection = new mySql({
   database: "BotDB",
 });
 
-var botsyst = connection.query("SELECT * from Boter");
-
 var passord = connection.query("SELECT * from login");
 
+function login() {
+  var bruker = document.getElementById("bruker");
+  var passord = document.getElementById("passord");
 
-function login(){
-  var bruker = document.getElementById("bruker")
-  var passord = document.getElementById("passord")
-  
   var loginnavn = "SELECT * from login WHERE " + bruker + "= '" + passord + "'";
 
-var logetin = connection.query(loginnavn);
-  if (logetin < 1){
+  var logetin = connection.query(loginnavn);
+  if (logetin < 1) {
     console.log("det fungerer å loge in");
-  } else if (logetin === 0){
+  } else if (logetin === 0) {
     console.log("ingen med det navnet");
   }
-
 }
 
-function displaymembers(){
-  var display = document.getElementById("liste")
+function displaymembers() {
+  var visfolk = connection.query("SELECT * from login");
 
-  var vis = connection.query("SELECT * from login")
-  
-  for (var i = 0; i < vis.length; i++) {
-
+  for (var i = 0; i < visfolk.length; i++) {
+    const HTMLString =
+      "<tr>" +
+      "<td>" +
+      visfolk[i].brukernavn +
+      "</td>" +
+      "<td>" +
+      visfolk[i].boter +
+      "</td>" +
+      "<td>" +
+      visfolk[i].ubetalt +
+      "</td>" +
+      "</tr>";
+    document.getElementById("score").innerHTML += HTMLString;
   }
 }
 
