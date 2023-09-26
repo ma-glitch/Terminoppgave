d9f735254d9af907259e2b85ff7adbc981e0f56e
Martin_T <Tangenmartin06@gmail.com>
2023-09-26 02:46:53 -0700

    php

--- Bot_system/index.php
+++ Bot_system/index.php
@@ -1,3 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Admin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
