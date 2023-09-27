18f11adcaa4548658b09efe3feb2c3a3717406ad
Martin_T <Tangenmartin06@gmail.com>
2023-09-27 00:49:09 -0700

    ape

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "Admin";
+$dbname = "BotDB";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
