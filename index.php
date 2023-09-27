dbe5169f30734c59654e21c42b44ceeaf0d047bb
Martin_T <Tangenmartin06@gmail.com>
2023-09-27 00:43:10 -0700

    ape

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "Admin";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
