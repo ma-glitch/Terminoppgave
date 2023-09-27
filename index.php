1d9fbe8efa36046b41f51068852a56d580ae0b84
Martin_T <Tangenmartin06@gmail.com>
2023-09-27 01:43:12 -0700

    cONNECTOR

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $password = "Admin";
 $dbname = "BotDB";
 
 // Create connection
-$conn = new  mysqli_connect($servername, $username, $password, $dbname);
+$conn =mysql_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
