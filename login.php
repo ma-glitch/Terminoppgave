1b044214d0163d43b7308cfb0b8a0c679e4b1be5
Martin_T <Tangenmartin06@gmail.com>
2023-09-29 01:07:22 -0700

    deded

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
  session_start();
-
+ 
+ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
 
 require_once "config.php";
 // Initialize error variables
