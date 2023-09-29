6af020fb2b73e9ccec8dbb30db6ddd2e83e13fa0
Martin_T <Tangenmartin06@gmail.com>
2023-09-29 02:03:34 -0700

    js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 function betalbtn() {
   document.getElementById("betal").style.display = "block";
 }
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
