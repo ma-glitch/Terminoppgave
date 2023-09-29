47c9d3e18316d9b04341c90ff5438e302417709f
NickName-14 <vikja001@outlook.com>
2023-09-29 11:40:16 +0200

    ads

--- leggtil.php
+++ leggtil.php
@@ -41,7 +41,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             echo "<div id='result'>Bot Verdi: $botValue</div>";   
             $sql2 = "SELECT bruker, total, ubetalt, FROM login WHERE bruker = '".$_SESSION["bruker"]."' ";
             
-            $sql2->bind_result($bruker, $total, $ubetalt);
+            if($stmt2 = $link->prepare($sql2))
+
+            $stmt2->bind_result($bruker, $total, $ubetalt);
             echo $sql2;
             echo $bruker;
             echo $total;
