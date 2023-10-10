51111fa5693a98b53b4e9579a9083c75a370951f
Martin_T <Tangenmartin06@gmail.com>
2023-10-10 04:29:13 -0700

    fix

--- login.php
+++ login.php
@@ -72,12 +72,9 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="style.css">
-    <style>/*
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    */</style>
 </head>
 <body>
     <div class="wrapper">
