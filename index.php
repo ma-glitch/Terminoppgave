9272e11f3559c136b1ffda696a85aa85eb37fcd0
Martin_T <Tangenmartin06@gmail.com>
2023-10-11 00:15:16 -0700

    deded

--- index.php
+++ index.php
@@ -67,7 +67,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
           <th>Total</th>
           <?php 
           $count = "SELECT SUM(total) FROM login";
-          
         
         echo "<td>" . $count . "</td>";
 ?>
