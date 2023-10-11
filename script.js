4159738180378f277d1b09760d915bcba1dc7d53
NickName-14 <vikja001@outlook.com>
2023-10-11 10:00:24 +0200

    qadwsfegykj

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function betalbtn() {
   document.getElementById("betal").style.display = "block";
-  document.getElementById("betal").style.display = "none";
   setTimeout(close(), 10000);
 }
 
 
 
 function close() {
+  console.log("timer funker");
   document.getElementById("betal").style.display = "none";
 }
