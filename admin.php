132714050826f8fbdd99f287aee3debe111fe15d
Martin_T <Tangenmartin06@gmail.com>
2023-10-12 05:14:43 -0700

    formatting

--- admin.php
+++ admin.php
@@ -2,63 +2,67 @@
 session_start();
 require_once "config.php";
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+  header("location: login.php");
+  exit;
 }
-if(!isset($_SESSION["admin"]) || $_SESSION["admin"] !== "yes"){
+if (!isset($_SESSION["admin"]) || $_SESSION["admin"] !== "yes") {
   header("location: index.php");
-  echo("du er ikke admin");
+  echo ("du er ikke admin");
   exit;
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bot system Linje-5</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <link rel="icon" type="image/x-icon" href="linje5.jpg">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bot system Linje-5</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="icon" type="image/x-icon" href="linje5.jpg">
 </head>
+
 <body>
 
-    <ul class="topnav">
-        <li><a  href="index.php">Hjem</a></li>
-        <li><a href="leggtil.php">Legg til bot</a></li>
-        <li><a class="active" href="admin.php">Admin</a></li>
-      </ul>
-      <h1 class="velkommen">Velkommen administrator <?php echo($_SESSION["navn"]);?>!</h1>
-      
-      <div class="updatebuttons">
-        <button onclick="opptnavn()">Oppdater navn</button>
-        <button onclick="oppttotal()">Oppdater total</button>
-        <button onclick="opptubetalt()">Oppdater ubetalt</button>
-      </div>
+  <ul class="topnav">
+    <li><a href="index.php">Hjem</a></li>
+    <li><a href="leggtil.php">Legg til bot</a></li>
+    <li><a class="active" href="admin.php">Admin</a></li>
+  </ul>
+  <h1 class="velkommen">Velkommen administrator
+    <?php echo ($_SESSION["navn"]); ?>!
+  </h1>
 
-      <table id="score">
-        <tr>
-          <th class="TABLE">Navn</th>
-          <th class="TABLE">Total</th>
-          <th class="TABLE">Total ubetalt</th>
-          <th class="TABLE">Oppdatere tabel</td>
-        </tr>
-        <?php
-        // Define the SQL query to retrieve data from your table
-        $sql = "SELECT navn, total, ubetalt FROM login ORDER BY CONVERT(total,INTEGER) DESC";
+  <div class="updatebuttons">
+    <button onclick="opptnavn()">Oppdater navn</button>
+    <button onclick="oppttotal()">Oppdater total</button>
+    <button onclick="opptubetalt()">Oppdater ubetalt</button>
+  </div>
 
-        // Execute the query
-        $result = $link -> query($sql);
-        
-        // Check if there are rows in the result
-        if ($result->num_rows > 0) {
-            // Output data of each row
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td class='TABEL'>" . $row["navn"] . "</td>";
-                echo "<td class='TABEL'>" . $row["total"] . "</td>";
-                echo "<td class='TABEL'>" . $row["ubetalt"] . "</td>";
-                echo "<td class='TABEL'><form method='post' action='update.php' class='updateform'>
+  <table id="score">
+    <tr>
+      <th class="TABLE">Navn</th>
+      <th class="TABLE">Total</th>
+      <th class="TABLE">Total ubetalt</th>
+      <th class="TABLE">Oppdatere tabel</td>
+    </tr>
+    <?php
+    // Define the SQL query to retrieve data from your table
+    $sql = "SELECT navn, total, ubetalt FROM login ORDER BY CONVERT(total,INTEGER) DESC";
+
+    // Execute the query
+    $result = $link->query($sql);
+
+    // Check if there are rows in the result
+    if ($result->num_rows > 0) {
+      // Output data of each row
+      while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td class='TABEL'>" . $row["navn"] . "</td>";
+        echo "<td class='TABEL'>" . $row["total"] . "</td>";
+        echo "<td class='TABEL'>" . $row["ubetalt"] . "</td>";
+        echo "<td class='TABEL'><form method='post' action='update.php' class='updateform'>
                 <input type='hidden' name='id' value='" . $row["navn"] . "'>
                 <input type='number' name='ubetalt' placeholder='" . $row["ubetalt"] . "'>
                 <input type='submit' name='submit' value='Oppdater' id='sumbit' class='oppdaterbtn'>
@@ -73,43 +77,44 @@ if(!isset($_SESSION["admin"]) || $_SESSION["admin"] !== "yes"){
                 <input type='text' name='navn' placeholder='" . $row["navn"] . "'>
                 <input type='submit' name='submit3' value='Oppdater' id='' class='oppdaterbtn'>
                 </form></td>";
-                echo "</tr>";
+        echo "</tr>";
         // Close the MySQLi connectio
-            }
-          }
-        ?>
-        <tr>
-          <th>Total</th>
-          <?php 
-          $count = "SELECT SUM(total) FROM login";
-          $result = $link->query($count);
-          //display data on web page
-          while($row = mysqli_fetch_array($result)){
-              echo "<td>". $row['SUM(total)']. "</td>";
-          }
-         ?>
-         </tr>
-      </table>
+      }
+    }
+    ?>
+    <tr>
+      <th>Total</th>
+      <?php
+      $count = "SELECT SUM(total) FROM login";
+      $result = $link->query($count);
+      //display data on web page
+      while ($row = mysqli_fetch_array($result)) {
+        echo "<td>" . $row['SUM(total)'] . "</td>";
+      }
+      ?>
+    </tr>
+  </table>
 
-<script>
+  <script>
 
-function opptnavn(){
-  document.querySelectorAll(".updateform").forEach(a=>a.style.display = "none");
-  document.querySelectorAll(".updateform3").forEach(a=>a.style.display = "block");
-  document.querySelectorAll(".updateform2").forEach(a=>a.style.display = "none");
-}
+    function opptnavn() {
+      document.querySelectorAll(".updateform").forEach(a => a.style.display = "none");
+      document.querySelectorAll(".updateform3").forEach(a => a.style.display = "block");
+      document.querySelectorAll(".updateform2").forEach(a => a.style.display = "none");
+    }
 
-function oppttotal(){
-  document.querySelectorAll(".updateform").forEach(a=>a.style.display = "none");
-  document.querySelectorAll(".updateform2").forEach(a=>a.style.display = "block");
-  document.querySelectorAll(".updateform3").forEach(a=>a.style.display = "none");
-}
-function opptubetalt(){
-  document.querySelectorAll(".updateform").forEach(a=>a.style.display = "block");
-  document.querySelectorAll(".updateform2").forEach(a=>a.style.display = "none");
-  document.querySelectorAll(".updateform3").forEach(a=>a.style.display = "none");
-}
+    function oppttotal() {
+      document.querySelectorAll(".updateform").forEach(a => a.style.display = "none");
+      document.querySelectorAll(".updateform2").forEach(a => a.style.display = "block");
+      document.querySelectorAll(".updateform3").forEach(a => a.style.display = "none");
+    }
+    function opptubetalt() {
+      document.querySelectorAll(".updateform").forEach(a => a.style.display = "block");
+      document.querySelectorAll(".updateform2").forEach(a => a.style.display = "none");
+      document.querySelectorAll(".updateform3").forEach(a => a.style.display = "none");
+    }
 
-</script>
+  </script>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,80 +1,86 @@
-    <?php
-    session_start();
-    require_once "config.php";
-    // Check if the user is logged in, if not then redirect him to login page
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: login.php");
-        exit;
-    }
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Bot system Linje-5</title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
-        <link rel="icon" type="image/x-icon" href="linje5.jpg">
-    </head>
-    <body onload="close()">
-        <ul class="topnav">
-            <li><a class="active" href="index.php">Hjem</a></li>
-            <li><a href="leggtil.php">Legg til bot</a></li>
-    <?php if(($_SESSION["admin"] == "yes")){
-        echo "<li><a href='admin.php'>Admin</a></li>";
-    }
+<?php
+session_start();
+require_once "config.php";
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bot system Linje-5</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="icon" type="image/x-icon" href="linje5.jpg">
+</head>
+
+<body onload="close()">
+    <ul class="topnav">
+        <li><a class="active" href="index.php">Hjem</a></li>
+        <li><a href="leggtil.php">Legg til bot</a></li>
+        <?php if (($_SESSION["admin"] == "yes")) {
+            echo "<li><a href='admin.php'>Admin</a></li>";
+        }
         ?>
-        </ul>
-
-        
-        <h1 class="velkommen">Velkommen <?php echo($_SESSION["navn"]);?>!</h1>
-<div class="betal">      <button class="betalebtn" onclick="betalbtn()">Betale Bot</button>
-      <h1 id="betal">Vipps</h1></div>
+    </ul>
 
 
+    <h1 class="velkommen">Velkommen
+        <?php echo ($_SESSION["navn"]); ?>!
+    </h1>
+    <div class="betal"> <button class="betalebtn" onclick="betalbtn()">Betale Bot</button>
+        <h1 id="betal">Vipps</h1>
+    </div>
 
-        <table id="score">
-            <tr>
+
+
+    <table id="score">
+        <tr>
             <th>Navn</th>
             <th>Total</th>
             <th>Total ubetalt</th>
-            </tr>
-            <?php
-            // Define the SQL query to retrieve data from your table
-            $sql = "SELECT navn, total, ubetalt FROM login ORDER BY CONVERT(total,INTEGER) DESC";
+        </tr>
+        <?php
+        // Define the SQL query to retrieve data from your table
+        $sql = "SELECT navn, total, ubetalt FROM login ORDER BY CONVERT(total,INTEGER) DESC";
 
-            // Execute the query
-            $result = $link -> query($sql);
-            
-            // Check if there are rows in the result
-            if ($result->num_rows > 0) {
-                // Output data of each row
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>" . $row["navn"] . "</td>";
-                    echo "<td>" . $row["total"] . "</td>";
-                    echo "<td>" . $row["ubetalt"] . "</td>";
-                    echo "</tr>";
-                }
-            } else {
-                echo "<tr><td colspan='3'>No records found</td></tr>";
+        // Execute the query
+        $result = $link->query($sql);
+
+        // Check if there are rows in the result
+        if ($result->num_rows > 0) {
+            // Output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["navn"] . "</td>";
+                echo "<td>" . $row["total"] . "</td>";
+                echo "<td>" . $row["ubetalt"] . "</td>";
+                echo "</tr>";
             }
+        } else {
+            echo "<tr><td colspan='3'>No records found</td></tr>";
+        }
 
-            // Close the MySQLi connectio
-            ?>
-            <tr>
+        // Close the MySQLi connectio
+        ?>
+        <tr>
             <th>Total</th>
-            <?php 
+            <?php
             $count = "SELECT SUM(total) FROM login";
             $result = $link->query($count);
             //display data on web page
-            while($row = mysqli_fetch_array($result)){
-                echo "<td>". $row['SUM(total)']. "</td>";
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<td>" . $row['SUM(total)'] . "</td>";
             }
             ?>
-            </tr>
-        </table>
+        </tr>
+    </table>
 
-        <script src="script.js"></script>
-    </body>
-    </html>
+    <script src="script.js"></script>
+</body>
+
+</html>
