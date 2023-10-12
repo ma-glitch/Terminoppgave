b9f71c6360b100acc49fa83a27a879ccf502ad5b
Martin_T <Tangenmartin06@gmail.com>
2023-10-12 01:45:50 -0700

    fix script

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function betalbtn() {
   var element = document.getElementById("betal");
   element.classList.toggle("betalblock");
-  setTimeout(close(), 100000);
 }
 
 
 
 function close() {
-  console.log("asdfafaa");
  var element2 = document.getElementById("betal");
  element2.classList.toggle("betal");
 }
