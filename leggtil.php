c5d5eff7ac8efce3567e1b5661f6c9c1870eed60
NickName-14 <vikja001@outlook.com>
2023-10-12 11:41:45 +0200

    fix

--- leggtil.php
+++ leggtil.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>leggtil</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="icon" type="image/x-icon" href="linje5.jpg">
 </head>
 <body>
