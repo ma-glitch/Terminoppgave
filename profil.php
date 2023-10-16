d96a43d8e9e986b9e81d9ddfcccf2157907f2f93
NickName-14 <vikja001@outlook.com>
2023-10-16 11:40:02 +0200

    deded

--- profil.php
+++ profil.php
@@ -33,8 +33,8 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                     <tr>
                         <td class="headerinfo">Navn:</td>
                         <?php
-                        echo "<td <input type='text' name='' placeholder='" . $_SESSION["navn"] . "'></td>";
-                        echo "<td <input type='submit' name='submit' value='' id='sumbit' class='oppdaterbtn'></td>"
+                        echo "<td><input type='text' name='' placeholder='" . $_SESSION["navn"] . "'></td>";
+                        echo "<td><input type='submit' name='submit' value='' id='sumbit' class='oppdaterbtn'></td>"
                         ?>
                     </tr>
                     <tr>
