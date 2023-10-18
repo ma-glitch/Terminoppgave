7feaf12ff45b960c8ca22668875b205079703a9c
Martin_T <Tangenmartin06@gmail.com>
2023-10-18 02:46:23 -0700

    dede

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once "config.php";
 // Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
 
 if (!isset($_COOKIE['bruker'])) {
     echo "cookie is not set";
