5237b2415a4f5639a0e789957ce471618b96bcc7
Martin_T <Tangenmartin06@gmail.com>
2023-10-18 00:29:07 -0700

    ined menu

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <body onload="close()">
     <ul class="topnav">
         <li><a href="index.php"><img src="Bilder/house-03-svgrepo-com.svg" class="menyicon" height="40px" width="auto"></a></li>
-        <li><a href="leggtil.html"><img src="Bilder/file-add-svgrepo-com.svg"class="menyicon" height="40px" width="auto"></a></li>
+        <li><a href="leggtil.php"><img src="Bilder/file-add-svgrepo-com.svg"class="menyicon" height="40px" width="auto"></a></li>
         <li><a href="profil.php"><img src="Bilder/user-01-svgrepo-com.svg" class="menyicon" height="40px" width="auto" ></a></li> 
         <?php if (($_SESSION["admin"] == "yes")) {
             echo "<li><a href='admin.php'><img src='Bilder/file-edit-svgrepo-com.svg' class='menyicon' height='40px' width='auto'></a></li>";
