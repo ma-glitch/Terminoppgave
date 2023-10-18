4106028949399cb0ca72b2238ce4257bbc245621
Martin_T <Tangenmartin06@gmail.com>
2023-10-18 00:20:33 -0700

    menu.hphp fix

--- menu.php
+++ menu.php
@@ -1,4 +1,8 @@
-<?php if (($_SESSION["admin"] == "yes")) {
+<?php 
+session_start();
+require_once "config.php";
+
+if (($_SESSION["admin"] == "yes")) {
             echo "<li><a href='admin.php'><img src='Bilder/file-edit-svgrepo-com.svg' class='menyicon' height='40px' width='auto'></a></li>";
-        }
+}
 ?>
