3bcdf19b94e14d202179cc5c2a7bc7c70a505b27
Martin_T <Tangenmartin06@gmail.com>
2023-11-30 12:53:13 +0100

    delete

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ $_SESSION = array();
 
 session_destroy();
 
-$sql = "DELETE FROM login WHERE bruker = " . $_SESSION['bruker'] . ";";
+$sql = "DELETE FROM login WHERE bruker = '" . $_SESSION['bruker'] . "';";
 
 setcookie("bruker", "", time() - 3600 * 24 * 30, "/");
 setcookie("pass", "", time() - 3600 * 24 * 30, "/");
