cb51001b1919347cb92dfba0404953eaca1214d3
NickName-14 <vikja001@outlook.com>
2023-11-30 13:37:42 +0100

    byby js

--- script.js
+++
@@ -1,9 +0,0 @@
-function betalbtn() {
-  console.log("test");
-  document.getElementById("betal").style.display = "block";
-}
-
-function close() {
- var element2 = document.getElementById("betal");
- element2.classList.toggle("betal");
-}
