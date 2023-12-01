b812a4adbf77722b01bc60669c82ff19ece49d66
NickName-14 <vikja001@outlook.com>
2023-12-01 11:07:35 +0100

    indexjul

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Bot system Linje-5</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="stylejul.css" />
     <link rel="icon" type="image/x-icon" href="Bilder/linje5.jpg">
 </head>
 
